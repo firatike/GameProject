9443127f89ba624e4e05c8d7c09c8f84c82518f3
firatike <firratt.1@hotmail.com>
2021-01-29 23:10:17 +0300

    Initial commit

---
+++ IUserValidationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    interface IUserValidationService
+    {
+        bool Validate(Gamer gamer); //UserValidationManager clasında true ve false döndüreceğimiz için void yerine bool yazıyoruz
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationService());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1998,
+                FirstName = "Fırat",
+                LastName = "İke",
+                IdentityNumber = 123456789,
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
